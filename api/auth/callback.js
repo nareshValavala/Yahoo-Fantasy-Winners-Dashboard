08181190e75fe5c4ceb3142ed7b9b26164c8eb3f
Naresh Valavala <vnvsnaresh@hotmail.com>
2025-08-21 14:54:00 -0400

    updated call back

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -1,10 +1,18 @@
-// api/auth/callback.js
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/html');
-  res.send(`
-    <script>
-      window.opener.postMessage(window.location.search, '*');
-      window.close();
-    </script>
-  `);
-}
+module.exports = async function handler(req, res) {
+  const { code, error } = req.query;
+  
+  console.log('Callback received:', { code: !!code, error });
+  
+  if (error) {
+    console.error('OAuth error:', error);
+    return res.redirect(`/?error=${encodeURIComponent(error)}`);
+  }
+  
+  if (code) {
+    console.log('Authorization code received, redirecting...');
+    return res.redirect(`/?code=${code}`);
+  }
+  
+  console.error('No code or error received');
+  res.redirect('/?error=missing_code');
+};
